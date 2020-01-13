ad4afc91d19e3c55e1896eff64211453e6797a9e
Paulin Lambert <paulin.lambert@gmail.com>
2020-01-13 14:53:35 +0100

    IMP CoupleOperator methods

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ class CoupleOperator:
         self.tuples = []
 
     def add(self, value1: int, value2: int):
-        pass
+        self.tuples.append((value1, value2))
 
     def sum(self):
-        pass
+        tuple_sum = 0
+        for t in self.tuples:
+            for value in t:
+                tuple_sum += value
+        return tuple_sum
+
+    def pop(self):
+        self.tuples.pop()

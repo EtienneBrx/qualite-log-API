ebac97ad8461c1eca0d7f9d4ddec3a771ccaf925
Etienne <e-pub456@laposte.net>
2020-01-13 14:40:47 +0100

    NEW add unit tests on sum function

--- main_test.py
+++ main_test.py
@@ -1,9 +1,28 @@
 import unittest
+from main import CoupleOperator
 
 
 class MainTest(unittest.TestCase):
-    def test_something(self):
-        pass
+    def test_sum_tuples_empty(self):
+        # liste vide
+        cp = CoupleOperator()
+        self.assertEqual(0, cp.sum())
+
+    def test_sum_tuples_one_couple(self):
+        cp = CoupleOperator()
+        cp.add(1,2)
+        self.assertEqual(3, cp.sum())
+
+    def test_sum_tuples_one_couple_with_negative(self):
+        cp = CoupleOperator()
+        cp.add(-6,2)
+        self.assertEqual(-4, cp.sum())
+
+    def test_sum_tuples_multiple_tuples(self):
+        cp = CoupleOperator()
+        cp.add(9, 7)
+        cp.add(-6, 2)
+        self.assertEqual(12, cp.sum())
 
 
 if __name__ == '__main__':

9f39b5b268641ce31d9f5d4f27714436748392b5
hugo <hugo.tamberi@free.fr>
2020-01-14 17:25:13 +0100

    IMP test game function

--- app/bowling.py
+++ app/bowling.py
@@ -1,6 +1,13 @@
 from app.bowling_api import BowlingScore
 
 
+def check_input(value):
+    if isinstance(value, int):
+        return 0 <= value <= 10
+    else:
+        return False
+
+
 class BowlingGame:
     def __init__(self):
         self.score = BowlingScore()
@@ -13,23 +20,17 @@ class BowlingGame:
 
     def request_score(self):
         score_val1 = int(input('first shot :'))
-        while not self.check_input(score_val1):
+        while not check_input(score_val1):
             score_val1 = int(input('Wrong value, Enter your score:'))
 
         score_val2 = 0
         if not score_val1 == 10:
             score_val2 = int(input('second shot :'))
-            while (not self.check_input(score_val2)) or score_val1 + score_val2 > 10:
+            while (not check_input(score_val2)) or score_val1 + score_val2 > 10:
                 score_val2 = int(input('Wrong value, re-enter second shot:'))
 
         self.score.add(score_val1, score_val2)
         print('Current score : ', self.score.sum())
-
-    def check_input(self, value):
-        if isinstance(value, int):
-            return 0 <= value and value <= 10
-        else:
-            return False
 
 
 if __name__ == "__main__":
--- test/bowling_test.py
+++ test/bowling_test.py
@@ -1,7 +1,38 @@
 import unittest
-from app.bowling_api import BowlingScore
+from app.bowling import BowlingGame, check_input
 
 
 class BowlingGameTest(unittest.TestCase):
-    if __name__ == '__main__':
-        unittest.main()
+    def test_check_input_invalid_bounds(self):
+        bowling = BowlingGame()
+        result = check_input(-6)
+        self.assertFalse(result)
+
+        result = check_input(11)
+        self.assertFalse(result)
+
+    def test_check_input_invalid_type(self):
+        bowling = BowlingGame()
+        result = check_input('A')
+        self.assertFalse(result)
+
+        result = check_input("test")
+        self.assertFalse(result)
+
+    def test_check_input_valid(self):
+        bowling = BowlingGame()
+        result = check_input(4)
+        self.assertTrue(result)
+
+    def test_check_input_valid_bounds(self):
+        bowling = BowlingGame()
+        result = check_input(0)
+        self.assertTrue(result)
+
+        result = check_input(10)
+        self.assertTrue(result)
+
+
+if __name__ == '__main__':
+    unittest.main()
+

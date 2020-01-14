92ae77c40388b316f7c70b263e903d0145412bb1
Etienne <e-pub456@laposte.net>
2020-01-14 16:49:05 +0100

    NEW add game functions

--- app/bowling.py
+++ app/bowling.py
@@ -1,3 +1,30 @@
+from app.bowling_api import BowlingScore
+
+
 class BowlingGame:
     def __init__(self):
+        self.score = BowlingScore()
         pass
+
+    def launch(self):
+        for i in range (10):
+            self.request_score()
+
+    def request_score(self):
+        pass
+        # print('Enter your score')
+        # score_val1 = input('first shot :')
+        # while not self.check_input(score_val1):
+        #     score_val1 = input('Wrong, Enter your score:')
+        #
+        # score_val2 = 0
+        # if not score_val1 == 10:
+        #     score_val2 = input('second shot :')
+        #     while not self.check_input(score_val2):
+        #         score_val2 = input('Wrong, Enter your score:')
+        #
+        # self.score.add(score_val1, score_val2)
+
+    def check_input(self):
+        pass
+

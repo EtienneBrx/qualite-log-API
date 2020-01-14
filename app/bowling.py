c2235c1de029ad0b3f64bb6f619301a05d0923ad
Etienne <e-pub456@laposte.net>
2020-01-14 17:17:35 +0100

    IMP implement game function

--- app/bowling.py
+++ app/bowling.py
@@ -4,27 +4,34 @@ from app.bowling_api import BowlingScore
 class BowlingGame:
     def __init__(self):
         self.score = BowlingScore()
-        pass
 
     def launch(self):
-        for i in range (10):
+        for i in range(10):
+            print('Round ', i, ': Enter your score')
             self.request_score()
+        print('> Total score', self.score.sum())
 
     def request_score(self):
-        pass
-        # print('Enter your score')
-        # score_val1 = input('first shot :')
-        # while not self.check_input(score_val1):
-        #     score_val1 = input('Wrong, Enter your score:')
-        #
-        # score_val2 = 0
-        # if not score_val1 == 10:
-        #     score_val2 = input('second shot :')
-        #     while not self.check_input(score_val2):
-        #         score_val2 = input('Wrong, Enter your score:')
-        #
-        # self.score.add(score_val1, score_val2)
+        score_val1 = int(input('first shot :'))
+        while not self.check_input(score_val1):
+            score_val1 = int(input('Wrong value, Enter your score:'))
 
-    def check_input(self):
-        pass
+        score_val2 = 0
+        if not score_val1 == 10:
+            score_val2 = int(input('second shot :'))
+            while (not self.check_input(score_val2)) or score_val1 + score_val2 > 10:
+                score_val2 = int(input('Wrong value, re-enter second shot:'))
 
+        self.score.add(score_val1, score_val2)
+        print('Current score : ', self.score.sum())
+
+    def check_input(self, value):
+        if isinstance(value, int):
+            return 0 <= value and value <= 10
+        else:
+            return False
+
+
+if __name__ == "__main__":
+    game = BowlingGame()
+    game.launch()
